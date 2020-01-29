72be3b975dfe1e7f887dfec7dd8ff131c6a83642
Jesse Gietzen <gietzenjesse@gmail.com>
2020-01-29 13:20:27 -0700

    add test 5

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,13 @@
 class School
-  attr_accessor :start_time, :hours_in_school_day, :student_names
+  attr_accessor :start_time, :hours_in_school_day
+  attr_reader :student_names
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
     @student_names = []
+  end
+
+  def add_student_name(new_student_name)
+    @student_names << new_student_name
   end
 end
